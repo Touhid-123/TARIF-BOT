a9c2b4ac7c8c001f05ec4e99b7641eddf5041b3c
MR-TARIF-YT130 <mrtarifyt.x130@gmail.com>
2024-11-19 20:55:42 +0600

    Create file.js

---
+++ scripts/cmds/file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "file",
+    version: "5.1.0",
+    author: "𝖠𝗋𝖸𝖺𝗇 🐔",
+    countDown: 5,
+    role: 2,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "𝗔𝗗𝗠𝗜𝗡",
+    guide: "{pn} file name."
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const fileName = args[0];
+    const permission = global.GoatBot.config.vipUser;
+ if (!permission.includes(event.senderID)) {
+ api.sendMessage("❌ | Only Tarif's vip user can use the command", event.threadID, event.messageID);
+ return;
+			}
+    if (!fileName) {
+      return api.sendMessage("😛 Please provide a file name.", event.threadID, event.messageID);
+    }
+
+    const fileArYan = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(fileArYan)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(fileArYan, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
