4d281bfe7b9ffb550fdb70976d3fbf7a63a84bc4
MR-TARIF-YT130 <mrtarifyt.x130@gmail.com>
2024-11-27 14:52:15 +0600

    Update touhid.js

--- scripts/cmds/touhid.js
+++ scripts/cmds/touhid.js
@@ -7,7 +7,7 @@ module.exports = {
 	 role: 0,
 	 shortDescription: "no prefix",
 	 longDescription: "no prefix",
-	 category: "no prefix",
+	 category: "𝗡𝗢 𝗣𝗥𝗘𝗙𝗜𝗫",
  },
 
  onStart: async function(){}, 
