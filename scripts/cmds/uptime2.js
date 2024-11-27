449685e8a38f1e8599880cf615ac9424bacec28f
MR-TARIF-YT130 <mrtarifyt.x130@gmail.com>
2024-11-27 10:51:14 +0600

    Update uptime2.js

--- scripts/cmds/uptime2.js
+++ scripts/cmds/uptime2.js
@@ -8,7 +8,7 @@ module.exports = {
         version: "1.2",
         author: "MR Rimon",
         countDown: 5,
-        role: 2,
+        role: 0,
         shortDescription: {
             en: ""
         },
